bbc8662a9c6e9abe307767d1ef5a86e7a82003da
Divya indur <divyaindur@gmail.com>
2020-08-25 21:44:34 +0530

    fisrt commit

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import React,{Component} from 'react';
+//let e1 =<h1>Hello My-app</h1>
+
+class App extends Component{
+    render(){
+        return <h1>Hello My-app Class Component</h1>
+    }
+}
+
+export default App;
---
+++ src/index.js
@@ -0,0 +1,5 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+//let e1 = React.createElement('h1',null,'hello My-app');
+ReactDOM.render(<App/>,document.getElementById('root'));
