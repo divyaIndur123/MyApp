bde0b2bb4e17f3ba22bcccf21940eb39e606fa27
Divya indur <divyaindur@gmail.com>
2020-08-25 22:38:46 +0530

    using angular bracket instead of React.fragment

--- src/App.js
+++ src/App.js
@@ -5,10 +5,10 @@ class App extends Component{
     render(){
 
         return(   
-        <React.Fragment>
+        <>
             <h1>Hello My-app Class Component 1</h1>
             <h1>Hello My-app Class Component 2</h1>
-        </React.Fragment> );
+        </> );
      
 
     }
