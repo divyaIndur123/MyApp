c4563693dc0590fafb3066265afd344968ce9d2a
Divya indur <divyaindur@gmail.com>
2020-08-25 22:44:34 +0530

    by importing fragment so that we neednot use React.Fragment all the time

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
-import React,{Component} from 'react';
-//let e1 =<h1>Hello My-app</h1>
+import React,{Component,Fragment} from 'react';
+
 
 class App extends Component{
     render(){
 
         return(   
-        <>
+        <Fragment>
             <h1>Hello My-app Class Component 1</h1>
             <h1>Hello My-app Class Component 2</h1>
             <h1>Hello My-app Class Component 2</h1>
-        </> );
+        </Fragment> );
      
 
     }
