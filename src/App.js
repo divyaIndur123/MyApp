be3c40449d8774100bdc2a10471b6be7e858e95d
Divya indur <divyaindur@gmail.com>
2020-08-26 14:10:40 +0530

    JSX with css attribute

--- src/App.js
+++ src/App.js
@@ -1,29 +1,6 @@
 import React from 'react';
+import './App.css';
+const el=<h1 className='bg'>Hello Divya</h1>
 
-//Simple String
-//const el=<h1>Hello Divya</h1>
+export default el;
 
-//Math Expresion
-//const el=<h1>{10+20}</h1>
-
-//Variable
-// const name = 'Bhavaya'
-// const el = <h1>Hello {name}</h1>
-
-//function
-// const show =()=>{
-//     return 'Hello Divya'
-// }
-
-// const el = <h1>{show()}</h1>
-
-
-//Property
-let user = {
-
-    firstname:'Divya'
-}
-
-const el = <h1>Hello {user.firstname}</h1>
-
- export default el;
--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import ReactDOM from 'react-dom';
+
 import el from './App';
 
 
