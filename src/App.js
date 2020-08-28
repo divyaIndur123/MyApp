fdd21074a1a2f3ea53e66cbd45a094906127e01e
Divya indur <divyaindur@gmail.com>
2020-08-28 14:53:16 +0530

    accessing children of component and commenting inside the jsx

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
 import React, { Component } from 'react';
-import PropTypes from 'prop-types'; //PropTypes or you can give which we want likt pt ot propsT
 class Student extends Component{
     render(){
         return(
             <div>
-                <h1>Hello {this.props.name}</h1>
-                <h1>your rollno is  {this.props.roll}</h1>
+                {/* we have to write the children to acces it */}
+                <h1>Hello {this.props.children}</h1> 
+                
             </div>
         );
     }
 }
 
-Student.propTypes={                         //here propTypes has to be same
-    name:PropTypes.string.isRequired,       // this is what we written in import here i have writen PropTypes if we write pt then
-    roll : PropTypes.number                 // pt.number and isRequired makes it compulsory for value
-};
 
-Student.defaultProps = {    // observe its props here
-    name:"defaultName"
-}
 export default Student;
 
-// isRequired not given and no value is given to the props.name then 
-//react doest shows any error so to get the warning in console if we miss to specify the value
--- src/index.js
+++ src/index.js
@@ -2,12 +2,5 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Student from './App';
 
-//  warning in console because name should be string as we have specified the prop-type same for roll as well
-// ReactDOM.render(<Student name={678} roll="101"/>,document.getElementById('root'));
-
-// isRequired will solve this problem by giving warning in console.
-//ReactDOM.render(<Student name roll={678}/>,document.getElementById('root'));
-
-// defaultProps will give the default value in these case
-ReactDOM.render(<Student roll={101}/>,document.getElementById('root'));
-
+//  observe the closing and tag , its the way slash is given
+ReactDOM.render(<Student>I am child</Student>,document.getElementById('root'));
