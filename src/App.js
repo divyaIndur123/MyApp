6d0a8dc2938911fc829e0d2970062d524d449061
Divya indur <divyaindur@gmail.com>
2020-09-03 17:00:56 +0530

    class component with state and setState() to manipulate the state

--- src/App.js
+++ src/App.js
@@ -1,42 +1,39 @@
-import React, { useState } from 'react';
+import React, { Component } from 'react';
 import Person from './Person/Person';
 
-const App = props => {
-   
-    const [personState, setPersonState] = useState({
-        persons: [
-        { name: 'Divya',age: 28},
-        { name: 'priya',age: 29},
-        { name: 'Bhavya',age: 25}
-    ]
-   });
+class App extends Component{
 
-   const switchNameHandler = () =>{
-    //console.log('was clicked');
-    //this.state.persons[0].name = 'Being Human';// cannot update in this way
-    setPersonState({
-        persons: [
-        { name: 'Diya',age: 28},
-        { name: 'priya',age: 29},
-        { name: 'Bhavya',age: 25}
-    ]
-});
-};
-   
+    state = {
+        person: [
+            {name: 'Divya',age: 25},
+            {name: 'Priya',age: 27},
+            {name: 'kriya',age: 24}],
+
+        otherProperty: 'somevalue'
+    }
+
+    updateNamesHandler = () => {
+        //console.log(this); // note: if orginal stste has more than onevalue than other state get merged untouched
+        this.setState({
+            person: [
+                {name: 'Diya',age: 25},
+                {name: 'Piya',age: 27},
+                {name: 'riya',age: 24}]
+
+        });
+    }
+    render(){
         return(
-            <div className="App">
-            <h1>Hello Event</h1>
-            <p>its working</p>
-            <button onClick={switchNameHandler}>Switch name</button>
-            <Person name={personState.persons[0].name}/>
-            <Person name={personState.persons[1].name}/>
-            <Person name={personState.persons[2].name}/>
-            
+            <div>
+                <Person name = {this.state.person[0].name} age = {this.state.person[0].age}/>
+                <Person name = {this.state.person[1].name} age = {this.state.person[1].age}/>
+                <Person name = {this.state.person[2].name} age = {this.state.person[2].age}/>
+                <button onClick = {this.updateNamesHandler} >Update-Names</button>
             </div>
         );
-    
-}
+    }
 
+}
 export default App;
 
 
